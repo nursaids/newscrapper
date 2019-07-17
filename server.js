32385de15930581e62ea71ebcb1cd181c2d88817
nursaid <uznet@yahoo.com>
2019-07-17 11:19:27 -0700

    server update)

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+
+
+const db = require("/models");
