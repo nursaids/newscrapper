e4347a11a30f6ca322a2a202fe5cc999ab1e5955
nursaid <uznet@yahoo.com>
2019-07-17 11:38:07 -0700

    updated article.js

---
+++ models/article.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ArticleSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    summary: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    readTime: {
+        type: String,
+        required: true
+    },
+    link: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    comments: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Comment"
+        }
+    ]
+    
+    
+    
+});
+
+const Article = mongoose.model("Article", ArticleSchema);
+
+module.exports = Article;
